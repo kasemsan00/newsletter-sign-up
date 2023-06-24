23ac482dade4c7a2515cd8e2b841645ea7ba0b77
kasemsan00 <kasemsan.cho@gmail.com>
2023-06-24 21:43:50 +0700

    dev

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,26 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <div className="text-red-500">Hello World</div>
-    </>
+    <div className="flex flex-col justify-center items-center h-screen bg-DarkSlateGrey">
+      <div className="text-4xl font-bold">Stay updated!</div>
+      <div>Join 60,000+ product manager receiving monthly updates on:</div>
+      <ul>
+        <li>Product discovery and building what matter</li>
+        <li>Measuring to ensure updates are a success</li>
+        <li>And much more!</li>
+      </ul>
+      <div>
+        <div>
+          <span>Email address</span>
+          <span>Valid email required</span>
+        </div>
+        <input type="email" />
+      </div>
+      <div>
+        <button>Subscribe to monthly newsletter </button>
+      </div>
+    </div>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
+
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "custom-yellow": "#BAA333",
+        Tomato: "hsl(4,100%,67%)",
+        DarkSlateGrey: "hsl(234,29%,20%)",
+        CharcoalGrey: "hsl(235,18%,26%)",
+        Grey: "hsl(231,7%,60%)",
+        White: "hsl(0,0%,100%)",
+      },
+    },
   },
   plugins: [],
 };
